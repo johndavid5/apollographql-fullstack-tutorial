75f1ee23a5c4ed4c01f00be5d4d68acb6bd0c0e4
John D. Aynedjian <john.david.aynedjian@gmail.com>
2020-02-12 20:03:32 -0500

    Start over, this time with index.tsx not index.jsx... (They _claim_ you can do *.jsx, but...)

---
+++ start/client/src/index.tsx
@@ -0,0 +1,31 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from "graphql-tag";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/'
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link
+});
+
+// ... above is the instantiation of the client object.
+client
+  .query({
+    query: gql`
+      query GetLaunch {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(`SHEMP: Moe, query result = `, result));
+
